9f7cd01546f421f68979d66628d1c82854077abe
ginnac <ginnacampbell1990@gmail.com>
2019-09-15 14:38:42 -0600

    server.js and models folder

---
+++ server.js
@@ -0,0 +1,44 @@
+//require packages
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+
+//require scraping packages
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+//require all models
+var db = require("./models");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+app.use(logger("dev"));
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//connect to mongo BD
+
+mongoose.connect("mongodb://localhost/scrapingNews", { useNewUrlParser: true });
+
+//routes...code below
+
+
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
